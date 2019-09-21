71a02cad123f4a80829dfd0d2d807c9b9268112c
pranjal <pranjal@192.168.0.102>
2019-09-21 13:27:20 +0530

    adding internationalization concept

--- src/main/java/com/thespringproject/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/thespringproject/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.thespringproject.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	//initial get
 	@RequestMapping(method=RequestMethod.GET, path="/helloworld")
@@ -25,5 +34,10 @@ public class HelloWorldController {
 		@GetMapping( path="/helloworld/pathVariable/{name}")
 		public HelloWorldBean helloWorldBean(@PathVariable String name) {
 			return new HelloWorldBean("Do you see my message"+" "+name+"?");
+		}
+		
+		@GetMapping( path="/helloworld-internationalized")
+		public String helloWorldInternationalized() {
+			return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 		}
 }
--- src/main/java/com/thespringproject/restfulwebservices/user/UserController.java
+++ src/main/java/com/thespringproject/restfulwebservices/user/UserController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.thespringproject.restfulwebservices.user.User;
+import com.thespringproject.restfulwebservices.user.UserDaoService;
 
 import sun.print.resources.serviceui;
 
@@ -31,6 +34,7 @@ public class UserController {
 		
 		User user = userDaoService.findUser(id);
 		
+		//Resource<User> resource<User>(user);
 		if (user==null)
 			throw new UserNotFoundException("id: "+id);
 		
